5a208a6d74ef00083c0e6039a7d9b6eb5db6500c
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-10-14 00:50:44 +0900

    真偽値

--- section2/object.rb
+++ section2/object.rb
@@ -9,3 +9,24 @@
 #
 # puts false.to_s
 
+# 複数行コメント
+=begin
+    コメント
+コメント
+=end
+
+# 変数名はスネークケースで書く
+special_price = 200
+puts special_price
+
+# rubyの真偽値
+# false またはnilであれば、false
+# それ以外はtrue
+
+find_data = nil
+data = find_data
+if data
+  puts 'データがあります。'
+else
+  puts 'データはありません'
+end
